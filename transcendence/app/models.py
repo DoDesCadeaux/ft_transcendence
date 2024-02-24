07cd1a38670819cb008082bf3cf8a5cd30368be7
DoDesCadeaux <dduraku@student.s19.be>
2024-02-24 17:06:10 +0100

    Link database with docker container

--- transcendence/app/models.py
+++ transcendence/app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    id_api = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100)
+    username = models.CharField(max_length=100, unique=True)
+    photo = models.ImageField(upload_to='user_photos/', blank=True, null=True)
+    play_time = models.DurationField()
+    state = models.CharField(max_length=50)  # Par exemple: online, offline, in-game
+
+class Tournament(models.Model):
+    name = models.CharField(max_length=100)
+    winner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+class Match(models.Model):
+    tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE, related_name="matches")
+    player1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='matches_as_player1')
+    player2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='matches_as_player2')
+    player1_score = models.IntegerField()
+    player2_score = models.IntegerField()
+    match_duration = models.DurationField()
+    winner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='matches_won')
+
--- transcendence/transcendence/settings.py
+++ transcendence/transcendence/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,11 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8e*xmi7jplc9$($ht!=2z@z9j=grw^sgjpp4*yh_n0q1y#wcq!'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'quaq%@(90hu(p1!syo*c$o8&871ngw(ee)(*^5fq!(g_f%g2%r')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', '0') == '1'
 
 ALLOWED_HOSTS = []
 
@@ -77,11 +77,11 @@ WSGI_APPLICATION = 'transcendence.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'transcendence_db',
-        'USER': 'dev',
-        'PASSWORD': '123',
-        'PORTS': 5432,
-        'HOST': 'db',
+        'NAME': os.environ.get('DB_NAME', 'transcendence_db'),
+        'USER': os.environ.get('DB_USER', 'dev'),
+        'PASSWORD': os.environ.get('DB_PASS', '123'),
+        'PORTS': os.environ.get('DB_PORTS', '5432'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
     }
 }
 
